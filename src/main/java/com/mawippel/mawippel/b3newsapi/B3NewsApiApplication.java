5b999c8850ca18ef9e5c062ef8240007d11afb16
Marcelo Wippel <mawippel2@hotmail.com>
2020-03-27 23:56:48 -0300

    initial commit

---
+++ src/main/java/com/mawippel/mawippel/b3newsapi/B3NewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.mawippel.mawippel.b3newsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B3NewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B3NewsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mawippel/mawippel/b3newsapi/B3NewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mawippel.mawippel.b3newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B3NewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
